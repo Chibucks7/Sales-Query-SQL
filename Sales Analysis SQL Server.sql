0dee79f5677f6a151aa6ccf7ea97e8e3ee95311d
Chibuike John (CJ) <112835030+Chibucks7@users.noreply.github.com>
2023-01-30 07:14:58 -0800

    Update Sales Analysis SQL Server.sql

--- Sales Analysis SQL Server.sql
+++ Sales Analysis SQL Server.sql
@@ -20,10 +20,10 @@ WHERE client.branch_id = (SELECT branch.branch_id
  FROM client
  WHERE client.branch_id = (SELECT branch.branch_id
                            FROM branch
-                           WHERE branch.mgr_id = (SELECT employee.emp_id
+                           WHERE branch.mgr_id = (SELECT TOP 1 employee.emp_id
                                                   FROM employee
                                                   WHERE employee.first_name = 'Michael' AND employee.last_name ='Scott'
-                                                  LIMIT 1));
+                                                  ));
 
 
 -- Find the names of employees who work with clients handled by the scranton branch
